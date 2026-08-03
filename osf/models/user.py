dfdff3de6edd84307344cca2e2bfa0da0ceffcf5
Taku Miyamoto <40813148+miyamoto83@users.noreply.github.com>
2026-08-03 18:10:54 +0900

    Fix OSFUser Django 3.2 issues: decode signed cookie bytes; check own contributor row in visible_contributor_to

--- osf/models/user.py
+++ osf/models/user.py
@@ -25,7 +25,7 @@ from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import PermissionsMixin
 from django.dispatch import receiver
 from django.db import models
-from django.db.models import Count
+from django.db.models import Count, Exists, OuterRef
 from django.db.models.signals import m2m_changed, post_save
 from django.utils import timezone
 from guardian.shortcuts import get_objects_for_user
@@ -52,7 +52,7 @@ from osf.models.tag import Tag
 from osf.models.mapcore import MAPProfile
 from osf.models.validators import validate_email, validate_social, validate_history_item
 from osf.utils.datetime_aware_jsonfield import DateTimeAwareJSONField
-from osf.utils.fields import NonNaiveDateTimeField, LowercaseEmailField
+from osf.utils.fields import NonNaiveDateTimeField, LowercaseEmailField, ensure_str
 from osf.utils.names import impute_names
 from osf.utils.requests import check_select_for_update
 from osf.utils.permissions import API_CONTRIBUTOR_PERMISSIONS, MANAGER, MEMBER, MANAGE, ADMIN
@@ -673,7 +673,11 @@ class OSFUser(DirtyFieldsMixin, GuidMixin, BaseModel, AbstractBaseUser, Permissi
         """
         Nodes where user is a bibliographic contributor (group membership not factored in)
         """
-        return self.nodes.filter(is_deleted=False, contributor__visible=True, type__in=['osf.node', 'osf.registration'])
+        # contributor__visible=True は独立 JOIN になり「いずれかの contributor が
+        # 可視」にマッチしてしまうため、本人の contributor 行に限定して判定する
+        return self.nodes.annotate(
+            self_is_visible=Exists(Contributor.objects.filter(node_id=OuterRef('id'), user_id=self.id, visible=True))
+        ).filter(is_deleted=False, self_is_visible=True, type__in=['osf.node', 'osf.registration'])
 
     @property
     def all_nodes(self):
@@ -1943,7 +1947,9 @@ class OSFUser(DirtyFieldsMixin, GuidMixin, BaseModel, AbstractBaseUser, Permissi
         secret = secret or settings.SECRET_KEY
 
         try:
-            token = itsdangerous.Signer(secret).unsign(cookie)
+            # itsdangerous の unsign は bytes を返す。Django 2.0+ は CharField への
+            # bytes 検索を文字列に強制しないため str へ戻す
+            token = ensure_str(itsdangerous.Signer(secret).unsign(cookie))
         except itsdangerous.BadSignature:
             return None
 
